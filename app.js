d83a143530d175c13de12a868156a82ebaa4fb96
Ana Moreno <anamariamoreno@gmail.com>
2018-10-20 20:10:26 +0200

    update signup

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+const profileRouter = require('./routes/profile');
 
-var app = express();
+const mongoose = require('mongoose');
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const app = express();
+
+// Connect to DataBase
+
+mongoose.connect('mongodb://localhost/jam-on', {
+  keepAlive: true,
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE
+})
+  .then(() => {
+    console.log('Connected to Mongo');
+  })
+  .catch(err => {
+    console.log('Error:', err);
+  });
+
+// Auth
+
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  secret: 'some-string',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}));
+
+// Makes the currentUser available in every page
+// note1: currentUser needs to match whatever you use in login/signup/logout routes
+// note2: if using passport, req.user instead
+app.use((req, res, next) => {
+  app.locals.currentUser = req.session.currentUser;
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,14 +65,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+app.use('/profile', profileRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ routes/auth.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup');
+});
+
+router.post('/signup', (req, res, next) => {
+  const newUser = req.body;
+  if (!newUser.username || !newUser.password) {
+    res.redirect('signup');
+  }
+  User.findOne({ username: newUser.username })
+    .then((user) => {
+      if (user) {
+        res.render('signup');
+      } else {
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashedPassword = bcrypt.hashSync(newUser.password, salt);
+        const registerUser = {
+          username: newUser.username,
+          password: hashedPassword
+        };
+        User.create(registerUser)
+          .then(() => {
+            res.redirect('profile'); // cambiar nombre de ruta y vista
+          })
+          .catch(next);
+      }
+    })
+    .catch(next);
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+module.exports = router;
