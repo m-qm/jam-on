4d8694082c3f44fa46c05727ff427129c91ab632
m-qm <mireiaquerol@gmail.com>
2018-10-22 21:37:52 +0200

    jams index + model

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.use('/jams', jamRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+  res.render('not-found');
 });
 
 // error handler
@@ -102,7 +102,6 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
--- routes/jams.js
+++ routes/jams.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const Jam = require('../models/jam');
 // const mongoose = require('mongoose');
 // const User = require('../models/user');
 // const bcrypt = require('bcrypt');
 // const saltRounds = 10;
 
 router.get('/', (req, res, next) => {
-  res.render('jams/jam-index', { page: 'jams', menuId: 'jams' });
+  Jam.find()
+    .then(jams => {
+      const data = {
+        jams: jams,
+        page: 'jams',
+        menuId: 'jams'
+      };
+
+      res.render('jams/index', data);
+    });
+});
+
+router.get('/add', (req, res, next) => {
+  res.render('jams/add', { page: 'jams', menuId: 'jams' });
+});
+
+router.post('/add', (req, res, next) => {
+  const newJam = req.body;
+  if (!newJam.title || !newJam.date || !newJam.city || !newJam.description || !newJam.style) {
+    return res.redirect('/jams/add');
+    // res.redirect('signup');
+  } else {
+    Jam.create(newJam)
+      .then(() => {
+        return res.redirect('/jams');
+      })
+      .catch(next);
+  }
 });
 
 module.exports = router;
