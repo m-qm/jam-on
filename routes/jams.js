089711c22ee10338c17a16b0f628ada6fd78fda2
m-qm <mireiaquerol@gmail.com>
2018-10-25 13:43:18 +0200

    favorites now working

--- routes/jams.js
+++ routes/jams.js
@@ -6,7 +6,6 @@ const ObjectId = mongoose.Types.ObjectId;
 const middlewares = require('../middlewares/middlewares');
 // const User = require('../models/user');
 
-// const mongoose = require('mongoose');
 // const User = require('../models/user');
 // const bcrypt = require('bcrypt');
 // const saltRounds = 10;
@@ -14,12 +13,24 @@ const middlewares = require('../middlewares/middlewares');
 /* ------------ Jam Index ----------------- */
 
 router.get('/', (req, res, next) => {
+  const objId = mongoose.Types.ObjectId(req.session.currentUser._id);
   Jam.find()
     .then(jams => {
+      for (let i = 0; i < jams.length; i++) {
+        for (let j = 0; j < jams[i].attendees.length; j++) {
+          if (jams[i].attendees[j].equals(objId)) {
+            jams[i].isAttending = true;
+          } else {
+            jams[i].isAttending = false;
+          }
+        }
+      }
       const data = {
         jams: jams,
         page: 'jams',
-        menuId: 'jams'
+        menuId: 'jams',
+        currentUser: req.session.currentUser
+        // isAttending: req.session.isAttending
       };
       res.render('jams/index', data);
     })
@@ -48,17 +59,24 @@ router.post('/add', (req, res, next) => {
 
 /* ----------- Atendees Jam ------------ */
 
-router.post('/:id/attendees', (req, res, next) => {
+router.post('/:id/attend', (req, res, next) => {
   const jamId = req.params.id;
   const userId = req.session.currentUser._id;
   // console.log(jamId, 'hey');
   // console.log(userId, 'ho');
   Jam.findById(jamId)
     .then(jam => {
-      jam.attendees.push(ObjectId(userId));
+      const position = jam.attendees.indexOf(ObjectId(userId));
+
+      if (position < 0) {
+        jam.attendees.push(ObjectId(userId));
+        req.flash('success', 'Added to attendees');
+      } else {
+        jam.attendees.splice(position, 1);
+        req.flash('danger', 'Removed from attendees');
+      }
       jam.save()
         .then((success) => {
-          req.flash('success', 'Saved to attendees');
           res.redirect('/jams');
         })
         .catch(next);
@@ -80,6 +98,8 @@ router.get('/:id/edit', (req, res, next) => {
     });
 });
 
+/* ----------- Edit Jam ------------ */
+
 router.post('/:id/save', middlewares.requireUser, (req, res, next) => {
   const id = req.params.id;
   const updateJam = req.body;
@@ -88,7 +108,6 @@ router.post('/:id/save', middlewares.requireUser, (req, res, next) => {
 
   Jam.findByIdAndUpdate(id, updateJam)
     .then((jam) => {
-      console.log('estoy aqui');
       res.redirect('/jams');
     })
     .catch(next);
@@ -107,38 +126,6 @@ router.post('/:id', (req, res, next) => {
     });
 });
 
-router.get('/:id', (req, res, next) => {
-  const id = req.params.id;
-  const jam = req.body;
-  Jam.findById(id, jam)
-    .then(jam => {
-      res.render('jams/jaminfo', { jam: jam });
-    })
-    .catch(error => {
-      console.log('error', error);
-      next(error);
-    });
-});
-
-/* ----------- Favorite Jam ------------ */
-
-router.post('/:id/favorites', middlewares.requireUser, (req, res, next) => {
-  const jamId = req.params.id;
-  const userId = req.session.currentUser._id;
-
-  Jam.findbyId(userId)
-    .then(user => {
-      user.favorites.push(ObjectId(jamId));
-      user.save()
-        .then((success) => {
-          req.flash('info', 'Añadido correctamente');
-          res.redirect('/jams');
-        })
-        .catch(next);
-    })
-    .catch(next);
-});
-
 /* --------- Delete a Jam ---------- */
 
 router.post('/:id/delete', (req, res, next) => {
@@ -146,8 +133,20 @@ router.post('/:id/delete', (req, res, next) => {
   Jam.findByIdAndDelete(id)
     .then(() => {
       req.flash('info', 'Borrado correctamente');
-
       res.redirect('/jams');
+    })
+    .catch(error => {
+      console.log('error', error);
+      next(error);
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const jam = req.body;
+  Jam.findById(id, jam)
+    .then(jam => {
+      res.render('jams/jaminfo', { jam: jam });
     })
     .catch(error => {
       console.log('error', error);
